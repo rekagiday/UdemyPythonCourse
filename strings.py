175bd076b364d27b5ff3080b613833234ff37485
Reka Giday <giday.r@gmail.com>
2018-08-15 10:29:39 +0100

    added old string formatting too (for python 3.5 and older)

--- strings.py
+++ strings.py
@@ -1,5 +1,9 @@
 name = "Réka"
 guess = 7
 
-answer = f"Nice try, {name}, but your guess of {guess} is wrong."
+answer = f"Nice try, {name}, but your guess of {guess} is wrong." #from python 3.6
 print(answer)
+
+guess += 1
+old_answer = "Nice try, {}, but your guess of {} is wrong.".format(name, guess) #older versions before 3.6
+print(old_answer)

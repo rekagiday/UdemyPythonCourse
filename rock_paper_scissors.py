90eaa74d1ca1ae3cae111042744a4522c7753f02
Reka Giday <giday.r@gmail.com>
2018-08-15 17:14:30 +0100

    rock paper scissors game refactoring

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,18 +3,16 @@ name1 = input('Player1 name:')
 name2 = input('Player2 name:')
 
 p1 = input(name1+ ", make your choice:")
-if (p1 and p1 == 'r' or p1 == 'p' or p1 == 's'):
-    print("Thank you!")
+if p1 and p1 == 'r' or p1 == 'p' or p1 == 's':
     p2 = input(name2+ ', make your choice:')
-    if (p2 and p2 == 'r' or p2 == 'p' or p2 == 's'):
-        print("Thank you!")
-        if (p1 == p2):
+    if p2 and p2 == 'r' or p2 == 'p' or p2 == 's':
+        if p1 == p2:
             print("Tie!")
-        elif(p1 == 'r' and p2 == 's') or (p1 == 'p' and p2 == 'r') or (p1 == 's' and p2 == 'p'):
+        elif (p1 == 'r' and p2 == 's') or (p1 == 'p' and p2 == 'r') or (p1 == 's' and p2 == 'p'):
             print(name1+' wins!')
-        elif(p1 == 'r' and p2 == 'p') or (p1 == 'p' and p2 == 's') or (p1 == 's' and p2 == 'r'):
+        else:
             print(name2 + ' wins!')
     else:
-        print('Please enter a valid answer (r, p or s)')
+        print('invalid answer (please choose: r, p, s)')
 else:
-    print('Please enter a valid answer (r, p or s)')
+    print('invalid answer (please choose: r, p, s)')

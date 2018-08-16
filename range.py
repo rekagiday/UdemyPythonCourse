7dd1f1ebfcca4c5070da88febb05be8635076e2e
Reka Giday <giday.r@gmail.com>
2018-08-16 18:01:31 +0100

    range in loops

--- range.py
+++ range.py
@@ -1,6 +1,11 @@
-for x in range(5): #from 0 to param(exlusive)
-    print(x)
-for y in range(1,6): #from param1 to param2(excl)
-    print(y)
-for z in range(2,11,2): #from param1 to param2(excl), by param3 steps
-    print(z)
+# for x in range(5): #from 0 to param(exlusive)
+#     print(x)
+# for y in range(1,6): #from param1 to param2(excl)
+#     print(y)
+# for z in range(2,11,2): #from param1 to param2(excl), by param3 steps
+#     print(z)
+
+x = input("How many times do I have to tell you? ")
+
+for i in range(int(x)):
+    print("Clean up your room!")

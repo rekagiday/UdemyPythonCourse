63ee701c63721f965301c1ab477b64fb882c12a6
Reka Giday <giday.r@gmail.com>
2018-08-17 17:35:47 +0100

    rock paper scrissors game v3.1, now you can add how many rounds needed to win the game

--- rock_paper_scissors2.py
+++ rock_paper_scissors2.py
@@ -3,9 +3,9 @@ from random import choice
 player_wins = 0
 computer_wins = 0
 print("****  ROCK  ****  PAPER  **** SCISSORS  **** \n type: r = rock, p = paper, s = scissors")
-name = input('player name: ')
-
-while player_wins < 3 and computer_wins < 3:
+winning_score = int(input("How many rounds you need to win to win the game? "))
+name = input('Player name: ')
+while player_wins < winning_score and computer_wins < winning_score:
     computer = choice(['r', 'p', 's'])
     player = input('Make your choice, ' + name+ ': ').lower()
     print('Computer\'s choice: ' + computer)
@@ -21,7 +21,7 @@ while player_wins < 3 and computer_wins < 3:
     else:
         print('Invalid player input')
 print("Game over!")
-if player_wins == 3:
+if player_wins > computer_wins:
     print(f"The winner is: {name}!")
 else:
     print("The winner is the Computer!")

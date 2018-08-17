65e8780be18556c97c1232d2159dabdb504122e7
Reka Giday <giday.r@gmail.com>
2018-08-17 17:26:08 +0100

    rock paper scrissors game v3, now it goes on until 3 wins

--- rock_paper_scissors2.py
+++ rock_paper_scissors2.py
@@ -1,16 +1,27 @@
 from random import choice
 
-print("****  ROCK  ****  PAPER  **** SCISSORS  **** \n  r = rock, p = paper, s = scissors")
+player_wins = 0
+computer_wins = 0
+print("****  ROCK  ****  PAPER  **** SCISSORS  **** \n type: r = rock, p = paper, s = scissors")
 name = input('player name: ')
-computer = choice(['r', 'p', 's'])
-player = input('Make your choice, ' + name+ ': ')
-print('Computer\'s choice: ' + computer)
-if player and player == 'r' or player == 'p' or player == 's':
-    if player == computer:
-        print('Tie!')
-    elif player == 'r' and computer == 's' or player == 'p' and computer == 'r' or player == 's' and computer == 'r':
-        print(name + ' wins!')
+
+while player_wins < 3 and computer_wins < 3:
+    computer = choice(['r', 'p', 's'])
+    player = input('Make your choice, ' + name+ ': ').lower()
+    print('Computer\'s choice: ' + computer)
+    if player and player == 'r' or player == 'p' or player == 's':
+        if player == computer:
+            print('Tie!')
+        elif player == 'r' and computer == 's' or player == 'p' and computer == 'r' or player == 's' and computer == 'r':
+            player_wins += 1
+            print(name + ' wins!')
+        else:
+            computer_wins += 1
+            print('Computer wins!')
     else:
-        print('Computer wins!')
+        print('Invalid player input')
+print("Game over!")
+if player_wins == 3:
+    print(f"The winner is: {name}!")
 else:
-    print('Invalid player input')
+    print("The winner is the Computer!")

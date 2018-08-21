e8058d89ed97b9a2d13150822a14d37981a0609a
Reka Giday <giday.r@gmail.com>
2018-08-21 10:51:40 +0100

    dict clear, copy, pop, popitem, fromkeys and update method exercises

--- basics/dictionaries/methods.py
+++ basics/dictionaries/methods.py
@@ -8,4 +8,21 @@ stuff = dict.fromkeys(['name', 'price'], 'unknown')
 student.clear() # clears the dict
 
 person.get('age') # 18
-person.get('score') # None 
+person.get('score') # None
+
+# pop: key must be provided!
+user.pop('score')
+print(user)
+
+#popitem() : removes a random key-val pair (usually the last given)
+user.popitem()
+print(user)
+
+# update() : adds a dict to an existing dict
+location = dict.fromkeys(['country', 'city', 'address'], 'unknown')
+person.update(location)
+print(person)
+
+# update also overwrites the items if key already exists in the dict
+person.update(stuff)
+print(person)

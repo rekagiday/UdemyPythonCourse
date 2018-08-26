97ff9d636d5c4f8546570eb0e818380acc3da15d
Reka Giday <giday.r@gmail.com>
2018-08-26 19:28:23 +0100

    docstring

--- basics/functions/basics.py
+++ basics/functions/basics.py
@@ -2,10 +2,12 @@ def generate_evens():
     return list(range(2,50,2))
 
 def raise_to_power(num, power = 2):
+    """raise_to_power(num, power): raises num to a specified power, power defaults to 2 """
     return num**power
 print(raise_to_power(3,3))
 print(raise_to_power(7))
 print(raise_to_power(power = 3, num = 5))
+print(raise_to_power.__doc__)
 
 def add(a, b):
     return(a + b)

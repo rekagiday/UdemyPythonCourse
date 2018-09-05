570e7f60fc1b897e9f97d92e096d899aeab0522a
Reka Giday <giday.r@gmail.com>
2018-09-05 16:32:57 +0100

    added list comprehension solution to filtering exercise

--- basics/functions/filters_and_maps_exercise.py
+++ basics/functions/filters_and_maps_exercise.py
@@ -5,4 +5,8 @@ names = ['Lassie', 'Blue', 'Jamie', 'Colt']
 your_instructors = list(map(lambda name: f"Your instructor is: {name}",
     filter(lambda val : val in instructors, names)))
 
+#same with list comprehension:
+your_instructors2 = [f"Your instructor is: {n}" for n in names if n in instructors]
+
 print(your_instructors)
+print(your_instructors2)

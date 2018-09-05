560bf3ce178b54ae68f39a715f65a2f22e4eb2e1
Reka Giday <giday.r@gmail.com>
2018-09-05 16:14:13 +0100

    filter example

---
+++ basics/functions/filter.py
@@ -0,0 +1,9 @@
+numbers = [1,2,3,4,5,6,7,8]
+
+evens = list(filter(lambda x: x % 2 == 0, numbers))
+
+####
+
+names = ['Anna', 'Belle', 'Anthony', 'David', 'Colt']
+
+a_names = list(filter(lambda n: n[0] = 'A', names))

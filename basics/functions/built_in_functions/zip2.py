36c41ebe8bc6954a329893d272348e0b46745f70
Reka Giday <giday.r@gmail.com>
2018-09-07 17:12:53 +0100

    add avg score

--- basics/functions/built_in_functions/zip2.py
+++ basics/functions/built_in_functions/zip2.py
@@ -2,10 +2,10 @@ students = ['Bill', 'Amanda', 'Joe', 'Mary']
 midterms = [98, 72, 87, 37]
 finals = [90, 88, 92, 69]
 
-# grades is the highest score of the 2 tests
+grades = dict(zip(students, map(lambda pair: max(pair), zip(midterms, finals))))
 
-grades = zip(
-    students, map(lambda pair: max(pair), zip(midterms, finals))
-)
+print(grades)
 
-print(dict(grades))
+avg_grades = dict(zip(students, map(lambda score: ((score[0]+score[1])/2), zip(midterms, finals))))
+
+print(avg_grades)

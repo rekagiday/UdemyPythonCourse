5417d8188345d976d66501545132704e38b8c0fc
Reka Giday <giday.r@gmail.com>
2018-09-08 22:20:47 +0100

    add print text

--- basics/functions/built_in_functions/zip2.py
+++ basics/functions/built_in_functions/zip2.py
@@ -4,8 +4,8 @@ finals = [90, 88, 92, 69]
 
 grades = dict(zip(students, map(lambda pair: max(pair), zip(midterms, finals))))
 
-print(grades)
+print(f"Best grades: {grades}")
 
 avg_grades = dict(zip(students, map(lambda score: ((score[0]+score[1])/2), zip(midterms, finals))))
 
-print(avg_grades)
+print(f"Average grades: {avg_grades}")

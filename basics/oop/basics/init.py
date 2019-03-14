c9e9bba5973f71daecbe10b0c6e347985c80d8fb
Reka Giday <giday.r@gmail.com>
2019-03-14 12:01:32 +0000

    python oop exercise 3 - defining attributes of a class

--- basics/oop/basics/init.py
+++ basics/oop/basics/init.py
@@ -1,6 +1,10 @@
 class User:
-    def __init__(self):
-        print("A new user is created.")
-        
-user1=User()
-user2=User()
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+user1=User("Joe", "Smith", 73)
+user2=User("Anne", "Perry", 20)
+print(user1.first_name, user1.last_name)
+print(user2.first_name, user2.last_name)
